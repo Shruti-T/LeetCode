22a6ff57df549e23433f0864df2dd474eea0a987
Shruti Tiwari <sh03shruti@gmail.com>
2021-09-05 01:31:16 +0530

    5.Remove ele

--- Easy.py
+++ Easy.py
@@ -1,4 +1,4 @@
-# --------------------------------------------------Reverse integer-------------------------------------------
+# --------------------------------------------------Q1) Reverse integer------------------------------------------
 # Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
 # cases = 1032, Runtime: 16 ms ,Memory Usage: 13.4 MB
 
@@ -30,7 +30,7 @@
 #         else:
 #             print(f)
 
-# ------------------------------------------------Palindrome----------------------------------------------------
+# ---------------------------------------------Q2) Palindrome----------------------------------------------------
 # An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
 # cases = 11510, Runtime: 80 ms, Memory Usage: 13.3 MB
 
@@ -47,7 +47,7 @@
 #     final = True if ori==rev else False
 #     print(final)
 
-# -------------------------------------------------Sum--------------------------------------------------------
+# -----------------------------------------------Q3) Sum--------------------------------------------------------
 # Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 # cases = 55, Runtime: 7519 ms, Memory Usage: 13.9 MB
@@ -72,18 +72,35 @@
 # ans = [second,one]
 # print(ans)
 
-# -------------------------------------------Remove Duplicates----------------------------------------------
+# -------------------------------------------Q4) Remove Duplicates----------------------------------------------
 # Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. 
 
 # cases = 362,Runtime: 60 ms,Memory Usage: 15.2 MB
 
-nums = [-1,-1,0,0]
-ori = len(nums)
-if (ori>0 ):
-    nums.append(-101)
-for i in range(0, ori):
-    if (nums[i] != nums[i+1]):
-        nums.append(nums[i])
+# nums = [-1,-1,0,0]
+# ori = len(nums)
+# if (ori>0 ):
+#     nums.append(-101)
+# for i in range(0, ori):
+#     if (nums[i] != nums[i+1]):
+#         nums.append(nums[i])
 
-del nums[0:ori+1]
+# del nums[0:ori+1]
+# print(nums)
+
+# ------------------------------------------------Q5) Remove Element--------------------------------------------
+# Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
+
+#  cases: 113 , Runtime: 16 ms, Memory Usage: 13.2 MB
+
+nums = [2,2,2,2,5,3,6,8,3]
+val = 3
+i=0
+while i<len(nums):
+    if(nums[i] == val):
+        nums.pop(i)
+        i=0
+    else:
+        i +=1
+
 print(nums)
