443d45391c05ecea8344482809ff8a683af730fb
Shruti Tiwari <sh03shruti@gmail.com>
2021-11-18 19:06:04 +0530

    Longest Common Prefix

--- Easy.py
+++ Easy.py
@@ -271,30 +271,33 @@
 
 
 # --------------------------------------- Q16)Longest Common Prefix -----------------------
+# Write a function to find the longest common prefix string amongst an array of strings. If there is no common prefix, return an empty string "".
 
-strs = ["a","a","b"]
-wordLen = []
-match = ""
-for i in strs:
-    wordLen.append(len(i))
+# cases: 123, Runtime: 32 ms, Memory Usage: 13.5 MB
 
-if(len(strs)<2 or strs[0] == ""):
+strs = ["a","ab"]
+commonTwo = ""
+if (len(strs) == 1):
     print(strs[0])
-elif(len(strs)>1):
-    for i in range(1,min(wordLen)+1):
+else:
+    for i in range(0, len(strs[0])+1):
         if(strs[0][0:i] == strs[1][0:i]):
-            match = strs[0][0:i] 
-    if(len(strs) == 2):
-        print(match)
+            commonTwo = strs[0][0:i]
+    if(len(strs) >2):
+        comLen = len(commonTwo)
+        ori = commonTwo
+        for k in range (2,len(strs)):
+            if (strs[k] != ""):
+                for j in range(1, comLen+1):
+                    if(ori[0:j] == strs[k][0:j]):
+                        commonTwo = ori[0:j]
+                    elif(ori[0:1] != strs[k][0:1]):
+                        commonTwo = ""
+            else:
+                commonTwo= ""
+            ori = commonTwo
+print(commonTwo)
 
 
-if(len(strs)>2):
-    if(match != ""):
-        for j in range(2,len(strs)):
-            dup = match
-            for i in range(1,len(dup)+1):
-                if(dup[0:i] == strs[j][0:i]):
-                    match = strs[j][0:i]
-        print(match)
-    else:
-        print("")
+
+    
