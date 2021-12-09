12483c3e16b80321f7a9c9fd16ff4b63d9a4d9ed
Shruti Tiwari <sh03shruti@gmail.com>
2021-12-09 21:45:57 +0530

    min stack

--- Easy.py
+++ Easy.py
@@ -299,4 +299,38 @@
 # print(commonTwo)
 
 
-    
+# ------------------------------------ Q17) ----------------
+
+
+class MinStack(object):
+   min=float('inf')
+   def __init__(self):
+      self.min=float('inf')
+      self.stack = []
+   def push(self, x):
+      if x<=self.min:
+         self.stack.append(self.min)
+         self.min = x
+      self.stack.append(x)
+   def pop(self):
+      t = self.stack[-1]
+      self.stack.pop()
+      if self.min == t:
+         self.min = self.stack[-1]
+         self.stack.pop()
+   def top(self):
+      return self.stack[-1]
+   def getMin(self):
+      return self.min
+
+obj = MinStack()
+obj.push(-2)
+obj.push(0)
+obj.push(-3)
+param_4 = obj.getMin()
+print(param_4)
+obj.pop()
+param_3 = obj.top()
+param_4 = obj.getMin()
+
+print(param_3, param_4)
