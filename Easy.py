e4e6d4eced4597e2723d5e9004ec1049becfc001
Shruti Tiwari <sh03shruti@gmail.com>
2021-12-19 22:09:32 +0530

    update

--- Easy.py
+++ Easy.py
@@ -359,3 +359,4 @@
 # print(y)
 
 # ---------------------------------------- Q19) Climbing stairs --------------------------------------------
+# You are climbing a staircase. It takes n steps to reach the top. Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
