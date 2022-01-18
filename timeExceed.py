515527e863ff1cc71d5c8109d0451da9bc1ca6ed
Shruti Tiwari <sh03shruti@gmail.com>
2022-01-18 17:46:03 +0530

    update

--- timeExceed.py
+++ timeExceed.py
@@ -1,13 +1,13 @@
 # TO TRY AGAIN
 # ---------------------------------------------Q7) Single one----------------------------------------------------
 
-nums = [9,1,2,3,1,2,3,4,0,4,0]
-nums.sort()
-nums.append(-4 * 10000)
-for i in range(0,len(nums)):
-    if(i%2 == 0 or i==0):
-        if(nums[i] != nums[i+1]):
-            x = nums[i]
-            break
+# nums = [9,1,2,3,1,2,3,4,0,4,0]
+# nums.sort()
+# nums.append(-4 * 10000)
+# for i in range(0,len(nums)):
+#     if(i%2 == 0 or i==0):
+#         if(nums[i] != nums[i+1]):
+#             x = nums[i]
+#             break
 
-print(x)
+# print(x)
